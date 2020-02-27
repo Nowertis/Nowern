bacf52091804420f3725db611e4242f53cc6edfa
Mariusz Woch <m.woch@multimedia.pl>
2020-02-27 13:18:46 +0100

    Craete new database: nowern. Use it instead of default one.

--- api/app.js
+++ api/app.js
@@ -8,18 +8,15 @@ const Post = require("./models/post");
 const app = express();
 
 mongoose
-  .connect("mongodb://docker:dockerpwd@mongo:27017/test", {
+  .connect("mongodb://docker:dockerpwd@mongo:27017/nowern", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => {
     console.log("Connected to database!");
-
-    initPosts();
   })
   .catch(err => {
     console.log("Connection failed!");
-    console.log(err);
   });
 
 app.use(cors());
@@ -54,37 +51,5 @@ app.delete("/posts/:id", (req, res) => {
     res.status(200).send({ message: "Post deleted" });
   });
 });
-
-var initPosts = function() {
-  new Post({
-    title: "Post1",
-    content: "Content 1"
-  }).save();
-
-  new Post({
-    title: "Post2",
-    content: "Content 2"
-  }).save();
-
-  new Post({
-    title: "Post3",
-    content: "Content 3"
-  }).save();
-
-  new Post({
-    title: "Post4",
-    content: "Content 4"
-  }).save();
-
-  new Post({
-    title: "Post5",
-    content: "Content 5"
-  }).save();
-
-  new Post({
-    title: "Post6",
-    content: "Content 6"
-  }).save();
-};
 
 module.exports = app;
--- mongoDb/mongo-init.js
+++ mongoDb/mongo-init.js
@@ -1,10 +1,21 @@
+db = db.getSiblingDB('nowern')
+
 db.createUser({
   user: "docker",
   pwd: "dockerpwd",
   roles: [
     {
       role: "readWrite",
-      db : "test"
+      db : "nowern"
     }
   ]
 });
+
+
+db.posts.insert({"title": "Test 1", "content": "Content 1"});
+db.posts.insert({"title": "Test 2", "content": "Content 2"});
+db.posts.insert({"title": "Test 3", "content": "Content 3"});
+db.posts.insert({"title": "Test 4", "content": "Content 4"});
+db.posts.insert({"title": "Test 5", "content": "Content 5"});
+db.posts.insert({"title": "Test 6", "content": "Content 6"});
+db.posts.insert({"title": "Test 7", "content": "Content 7"});
